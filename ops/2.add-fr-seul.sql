a078bda7164d294c7d0e22a1495d3a020b217c38
TOomaAh <TOomaAh>
2026-08-25 21:59:15 +0000

    add fr seul

---
+++ ops/2.add-fr-seul.sql
@@ -0,0 +1,9 @@
+-- @operation: export
+-- @entity: batch
+-- @name: add fr seul
+-- @exportedAt: 2026-08-25T21:59:13.790Z
+-- @opIds: 302
+
+-- --- BEGIN op 302 ( create regular_expression "[FR] seul" )
+insert into "regular_expressions" ("name", "pattern", "description", "regex101_id") values ('[FR] seul', '^\[FR\]', NULL, NULL);
+-- --- END op 302
