82c03699058ae9d5a40fc2004918b1e0e8d6f432
TOomaAh <TOomaAh>
2026-08-25 21:58:03 +0000

    regex fr seul

---
+++ ops/1.regex-fr-seul.sql
@@ -0,0 +1,17 @@
+-- @operation: export
+-- @entity: batch
+-- @name: regex fr seul
+-- @exportedAt: 2026-08-25T21:57:56.895Z
+-- @opIds: 298, 299, 300
+
+-- --- BEGIN op 298 ( delete regular_expression "[FR] seul" )
+delete from "regular_expressions" where "name" = '[FR] seul';
+-- --- END op 298
+
+-- --- BEGIN op 299 ( delete regular_expression "[FR] seul" )
+delete from "regular_expressions" where "name" = '[FR] seul';
+-- --- END op 299
+
+-- --- BEGIN op 300 ( delete regular_expression "[FR] seul" )
+delete from "regular_expressions" where "name" = '[FR] seul';
+-- --- END op 300
