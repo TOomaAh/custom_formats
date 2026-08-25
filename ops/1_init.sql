b5802278fc3948ed94059135b3160077856ac570
THOMAS B <tbrunet72@gmail.com>
2026-08-25 23:01:30 +0200

    Remove insert and create quality_profiles and radarr_naming tables
    Removed insert statement for quality_profiles and added table creation statements.

--- ops/1_init.sql
+++ ops/1_init.sql
@@ -1,2 +1,2 @@
-INSERT INTO quality_profiles (name, description, upgrades_allowed, minimum_custom_format_score, upgrade_until_score, upgrade_score_increment)
-VALUES ('1080p Balanced', '1080p Balanced targets consistent & immutable 1080p **WEB-DLs** using the Streaming Source and Audio Formats to determine the level of Transparency.
+CREATE TABLE IF NOT EXISTS 'quality_profiles';
+CREATE TABLE IF NOT EXISTS 'radarr_naming';
